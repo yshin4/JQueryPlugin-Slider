f9551ca9a1e1e9efb2946c57c274216410a8c7a5
yshin4 <yshin4@lion.lmu.edu>
2016-12-08 12:52:04 -0800

    slider working

--- jquery.div-slider.js
+++ jquery.div-slider.js
@@ -1,26 +1,57 @@
 
-
 (($) => {
-  $.fn.slider = function(options){
+    $.fn.slider = function (options) {
+        let $this = this;
+        let $current = null;
+        let currentPosition = 0;
 
-    let data = $('ul.data li');
-    let target;
+        $this.addClass("slider").mousedown((event) => {
+            $current = $(event.currentTarget);
+            currentTop = $current.offset().top;
+        });
 
-    data.hide().first().show();
+        $(document).mousemove((event) => {
+            if ($current) {
+                // let newCss = newPosition + "px;";
+                let newPosition = event.pageX
+                if(newPosition >= 460){ //max slider value
+                  newPosition = 438; //max slider value - knob width - borderlength*2
+                }else if(newPosition < 60){ //min slider value
+                  newPosition = 60;
+                }
+                $current.offset({top:currentTop, left: newPosition});
+                $current.data({ 'value' : newPosition});
 
-    function sliderResponse(target) {
-        data.fadeOut(300).eq(target).fadeIn(300);
-    }
+            }
+        }).mouseup(() => {
+            $current = null;
+        });
 
-    // $('#sliderBar').change(function() {
-    //     target = $('#sliderBar').val();
-    //     sliderResponse(target);
-    // });
-
-
-
-  };
-
+        return $this;
+    };
 })(jQuery);
+//
+// (($) => {
+//   $.fn.slider = function(){
+//
+//     let data = $('ul.data li');
+//     let target;
+//
+//     data.hide().first().show();
+//
+//     function sliderResponse(target) {
+//         data.fadeOut(300).eq(target).fadeIn(300);
+//     }
+//
+//     $('#sliderBar').change(function() {
+//         target = $('#sliderBar').val();
+//         sliderResponse(target);
+//     });
+//
+//
+//
+//   };
+//
+// })(jQuery);
 
-$('ul.data li').slider();
+$('.knob').slider();
