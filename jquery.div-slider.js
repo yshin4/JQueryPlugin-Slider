a097517bee5f8f684c9cc5618083f1aec30901df
yshin4 <yshin4@lion.lmu.edu>
2016-12-08 14:03:06 -0800

    knob has max and min value

--- jquery.div-slider.js
+++ jquery.div-slider.js
@@ -1,9 +1,14 @@
 
+
 (($) => {
-    $.fn.slider = function (options) {
+    $.fn.knob = function (options) {
         let $this = this;
         let $current = null;
         let currentPosition = 0;
+        let knobWidth = $('.knob').width();
+        let maxValue = $('.slider').width() + $('.slider').offset().left + 1;
+        let minValue = $('.slider').offset().left;
+
 
         $this.addClass("slider").mousedown((event) => {
             $current = $(event.currentTarget);
@@ -12,46 +17,46 @@
 
         $(document).mousemove((event) => {
             if ($current) {
-                // let newCss = newPosition + "px;";
                 let newPosition = event.pageX
-                if(newPosition >= 460){ //max slider value
-                  newPosition = 438; //max slider value - knob width - borderlength*2
-                }else if(newPosition < 60){ //min slider value
-                  newPosition = 60;
+                if(newPosition >= maxValue){
+                  newPosition = maxValue - knobWidth;
+                }else if(newPosition < minValue){
+                  newPosition = minValue;
                 }
                 $current.offset({top:currentTop, left: newPosition});
-                $current.data({ 'value' : newPosition});
 
             }
         }).mouseup(() => {
             $current = null;
+
         });
 
         return $this;
     };
 })(jQuery);
-//
-// (($) => {
-//   $.fn.slider = function(){
-//
-//     let data = $('ul.data li');
-//     let target;
-//
-//     data.hide().first().show();
-//
-//     function sliderResponse(target) {
-//         data.fadeOut(300).eq(target).fadeIn(300);
-//     }
-//
-//     $('#sliderBar').change(function() {
-//         target = $('#sliderBar').val();
-//         sliderResponse(target);
-//     });
-//
-//
-//
-//   };
-//
-// })(jQuery);
 
-$('.knob').slider();
+
+(($) => {
+  $.fn.slider = function(){
+
+    let data = $('ul.data li');
+    let target;
+
+    data.hide().first().show();
+
+    function sliderResponse(target) {
+        data.fadeOut(300).eq(target).fadeIn(300);
+    }
+
+    $('knob').change(function() {
+        target = $('knob').offset().left();
+        sliderResponse(target);
+    });
+
+  };
+
+})(jQuery);
+
+
+$('.knob').knob();
+$('ul.data li').slider();
